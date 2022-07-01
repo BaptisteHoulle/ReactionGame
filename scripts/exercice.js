5cacae0a20cd9893f149b78b0a17959ad1e7af96
Baptiste Houlle <bhoulle@conceptfactory.lu>
2022-07-01 11:25:03 +0200

    modif exercice

--- scripts/exercice.js
+++ scripts/exercice.js
@@ -1,27 +1,86 @@
-let randomNumber = Math.floor(Math.random() * 10) + 1;
-let essais = 3;
-let prompt = parseInt(window.prompt("Entrez un nombre entre 0 et 10"));
+(function () {
 
-while (prompt != randomNumber && essais > 0) {
-essais--;
-    if (prompt < randomNumber) {
-        console.log("Le chifffre à trouver est plus grand");
-    }
+        var afficherOnglet = function (a, ainmations) {
 
-    if (prompt > randomNumber) {
-        console.log("Le chifffre à trouver est plus petit");
-    }
+            if (ainmations === undefined) {
+                ainmations = true;
+            }
 
-    if (essais >  0) {
-        prompt = window.prompt("Re-essayer");
+            var div = a.parentNode.parentNode.parentNode;
+            var li = a.parentNode;
+            var activeTab = div.querySelector('.tab-content.active'); //contenu actif
+            var aAfficher = div.querySelector(a.getAttribute('href')); //contenu à afficher
 
-    }
-}
+            if (li.classList.contains('active')) {
+                return false;
+            }
 
-if (prompt == randomNumber) {
-    console.log("Bravo, vous avez gagné");
-} 
+            //On RETIRE la class active de l'onglet actif
+            div.querySelector('.tabs .active').classList.remove('active');
+            //J'ajoute la class active à l'onglet actuel
+            li.classList.add('active');
 
-else {
-    console.log("Perdu !");
-}
+            //On RETIRE la class active du contenu actif
+            //div.querySelector('.tab-content.active').classList.remove('active');
+
+            //j'ajoute la class active sur le contenu correspondant à mon clique
+            //div.querySelector(a.getAttribute('href')).classList.add('active');
+
+            if (ainmations) {
+                activeTab.classList.add('fade');
+                activeTab.classList.remove('in');
+
+                var transitionend = function () {
+                    this.classList.remove('fade');
+                    this.classList.remove('active');
+                    aAfficher.classList.add('active');
+                    aAfficher.offsetWidth;
+                    aAfficher.classList.add('fade');
+                    aAfficher.offsetWidth;
+                    aAfficher.classList.add('in');
+                    activeTab.removeEventListener('transitionend', transitionend);
+                    activeTab.removeEventListener('webkitTransitionEnd', transitionend);
+                    activeTab.removeEventListener('oTransitionEnd', transitionend);
+
+                };
+                activeTab.addEventListener('transitionend', transitionend);
+                activeTab.addEventListener('webkitTransitionEnd', transitionend);
+                activeTab.addEventListener('oTransitionEnd', transitionend);
+
+            } else {
+                aAfficher.classList.add('active');
+                activeTab.classList.remove('active');
+            }
+
+            // On ajoute la classe fadr sur l'élement actif
+            // à la fin de l'ainmation
+            //  on retire la classe fade et active
+            //  on ajoute la classe fade et active à l'élément à afficher
+            //  on ajoute la classe in
+        }
+
+        var tabs = document.querySelectorAll('.tabs a');
+        for (var i = 0; i < tabs.length; i++) {
+            tabs[i].addEventListener('click', function (e) {
+                afficherOnglet(this, true)
+            })
+        }
+
+        /*
+        JE RECUPERER Le hash
+        AJOUTER LA CLASS ACTIVE SUR LE LIEN href="hash"
+        RETIRER LA CLASS ACTIVE SUR les autres onglets
+        Afficher / Masquer les contenus correspondants
+        */
+
+        var hashChange = function (e) {
+            var a = document.querySelector('a[href="' + hash + '"]');
+            if (a !== nul && !a.classList.contains('active')) {
+                afficherOnglet(a, e !== undefined);
+            }
+        }
+
+        window.addEventListener('hashchange', hashChange);
+        hashChange();
+
+})();
